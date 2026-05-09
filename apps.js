37c569607be77945934272c20bf577a7e821162c
muhammed-ahmed877 <ahmeddehware@gmail.com>
2026-05-08 21:58:20 -0700

    practice

--- apps.js
+++ apps.js
@@ -12,44 +12,176 @@ console.log("💻 JavaScript Function Assignment")
 // 🧩 Questions:
 // 1. addTwoNumbers
 // Write a function that takes two numbers and returns their sum.
+// function num(v1, v2) {
+// return v1 + v2 
+
+// }
+
+// console.log(num(2,3));
+
+
 // 2. subtractNumbers
-// Create a function that takes two numbers and returns the result of the first number
-// minus the second.
+// Create a function that takes two numbers and returns the result of the first number minus the second.
+
+// function num(v1, v2) {
+//  return v1 - v2 ;
+// }
+
+// console.log(num(5, 6));
+
+
 // 3. multiplyNumbers
 // Write a function that multiplies two numbers and returns the result.
+
+
+// function num(v1 , v2)
+// {
+// return v1*v2
+// }
+// console.log(num(2,5));
+
+
+
 // 4. divideNumbers
-// Create a function that takes two numbers and returns the result of dividing the first by
-// the second.
+// Create a function that takes two numbers and returns the result of dividing the first by the second.
+// function num(v1,v2) {
+//     return v1 / v2
+// }
+// console.log(num(2,4));
+
 // 5. getRemainder
-// Write a function that takes two numbers and returns the remainder when the first
-// number is divided by the second.
+// Write a function that takes two numbers and returns the remainder when the first number is divided by the second.
+// function num(v1 , v2) {
+//     return v1% v2
+// }
+
+// console.log(num(2 ,3));
+
 // 6. isPositive
-// Write a function that checks if a number is positive. If it is, return "Positive",
-// otherwise return "Not Positive".
+// Write a function that checks if a number is positive. If it is, return "Positive", otherwise return "Not Positive".
+// function num(v1 ) {
+//     if (v1>=0) {
+//         return"positive"
+        
+//     } else {
+//         return "negative number"
+        
+//     }
+    
+    
+// }
+
+
+// console.log(num(-3 ));
+
 
 // 7. isNegative
-// Create a function that checks if a number is negative. If it is, return "Negative", else
-// return "Not Negative".
+// Create a function that checks if a number is negative. If it is, return "Negative", else return "Not Negative".
+// function num(v1) {
+//     if (v1 <= 0) {
+//         return"negative";
+        
+//     } else {
+//         return"not negative";
+        
+//     }
+    
+// }
+
+// console.log(num(-5));
+
 // 8. checkEven
-// Write a function that takes a number and returns "Even" if the number is even,
-// otherwise "Odd".
+// Write a function that takes a number and returns "Even" if the number is even, otherwise "Odd".
+// function num(v1) {
+//     if (v1 %2===0) {
+//         return"even";
+        
+//     } else {
+//         return"odd";
+        
+//     }
+    
+// }
+
+// console.log(num(2000002));
+
+
 // 9. isEqual
-// Write a function that takes two numbers and returns "Equal" if they are the same,
-// otherwise "Not Equal".
-// 10. compareNumbers
-// Create a function that returns "Greater" if the first number is greater than the second,
-// else return "Smaller".
+// Write a function that takes two numbers and returns "Equal" if they are the same, otherwise "Not Equal".
+// function num(v1,v2){
+//     if (v1===v2) {
+//         return"equal";
+//     } else {
+//         return"not equal";
+//     }
+
+// }
+
+
+
+// console.log(num(4,4));
+
+// // 10. compareNumbers
+// // Create a function that returns "Greater" if the first number is greater than the second,else return "Smaller".
+// function num(v1, v2){
+//     if (v1 > v2) {
+//         return"greater";
+        
+//     } else {
+//         return"smaller";
+        
+//     }
+// }
+// console.log(num(6,5));
+
+
 // 11. getFullName
 // Write a function that takes a first name and last name, and returns the full name.
+// function num(v1 , v2) {
+//     return v1 + " " + v2
+    
+// }
+// console.log(num("mohammed" , "Ahmed"));
+
 // 12. getGrade
 // Create a function that takes a number (0–100) and returns:
 // ● "Pass" if the score is 40 or more
 // ● "Fail" if it is less than 40
+// function num(v1) {
+//     if (v1 >= 40) {
+//         return "pass"
+//     } else {
+//        return "fail"
+//     }
+// }
+// console.log(num(39));
 // 13. isTeenager
-// Write a function that takes an age and returns "Teenager" if the age is between 13
-// and 19, otherwise return "Not a Teenager".
+// Write a function that takes an age and returns "Teenager" if the age is between 13 and 19, otherwise return "Not a Teenager".
+// function num (v1 ){
+//     if (v1>=13 && v1<=19 ) {
+//         return "teenager";
+        
+//     } else {
+//         return "not a teenager";
+        
+//     }
+// }
+// console.log(num(12));
+
 // 14. greetUser
 // Write a function that takes a name and returns a greeting like "Hello, Ahmed!".
+// function num(v1){
+
+//     return "hello," +v1 + " !" 
+    
+// }
+// console.log(num("mir Mohammed Ahmed Dehwar"));
+
+
+
+
+
+
 // 15. isEligibleToVote
 // Create a function that takes age and returns "Eligible" if age is 18 or above,
 // otherwise "Not Eligible".
